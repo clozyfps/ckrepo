d0abe03041ec3891ffe05105178cba4d43ef837f
NoiiLive <noiit@boii-underage>
2024-01-31 13:43:57 -0700

    added small buffs to adaption

---
+++ src/main/java/net/mcreator/craftkaisen/procedures/PlayerAdaptionBuffsProcedure.java
@@ -0,0 +1,48 @@
+package net.mcreator.craftkaisen.procedures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import net.mcreator.craftkaisen.network.CraftKaisenModVariables;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class PlayerAdaptionBuffsProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			execute(event, event.player);
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
+			return;
+		if (((entity.getCapability(CraftKaisenModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new CraftKaisenModVariables.PlayerVariables())).technique).equals("Adaption")) {
+			if (entity.getPersistentData().getBoolean("BurningAdapted") == true) {
+				if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 60, 0, false, false));
+			}
+			if (entity.getPersistentData().getBoolean("StarvingAdapted") == true) {
+				if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.SATURATION, 60, 0, false, false));
+			}
+			if (entity.getPersistentData().getBoolean("SuffocationAdapted") == true) {
+				if (entity instanceof LivingEntity _entity && !_entity.level.isClientSide())
+					_entity.addEffect(new MobEffectInstance(MobEffects.WATER_BREATHING, 60, 0, false, false));
+			}
+		}
+	}
+}
